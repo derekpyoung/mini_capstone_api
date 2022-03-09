fc4f69915ae346abf7c1cde89908dad7e0bb1ab1
Derek Young <derekpyoung12@gmail.com>
2022-03-09 10:29:56 -0800

    terminal jsongem install

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,10 +2,12 @@ class ProductsController < ApplicationController
 
   def products 
     render json: Product.all.to_json
+    p Product.all.to_json 
   end 
 
   def one_product
-    render json: Product.first.to_json 
+    render json: Product.last.to_json 
+    p Product.last.to_json 
   end 
 
 end
