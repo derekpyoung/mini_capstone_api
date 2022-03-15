415d11debc92d27539b25d30538b40b0b33b9b35
Derek Young <derekpyoung12@gmail.com>
2022-03-15 10:23:24 -0700

    update product model

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,30 +3,26 @@ class ProductsController < ApplicationController
   def show
     id = params[:id].to_i
     product = Product.find_by(id: id)
-    render json: {
-       product: product 
-    }
+    render json: product.as_json(methods: [:readable_created_at, :readable_updated_at,:is_discounted?,:tax, :total])
+    
   end 
 
   def index 
     products = Product.all 
-    render json: {
-      product: products
-    }
+    render json: products.as_json(methods: [:readable_created_at, :readable_updated_at,:is_discounted?,:tax, :total])
   end 
 
   def create 
     product = Product.new(name: params[:input_name], price: params[:input_price], img_url: params[:input_url], description: params[:input_description])
     product.save
-    render json: product.as_json
+    render json: product.as_json(methods: [:readable_created_at, :readable_updated_at])
   end 
 
   def destroy 
     id = params[:id]
     product = Product.find_by(id: id)
-    render json: {
-      Deleted: product 
-    }
+    render json: product.as_json
+    
     product.delete
   end 
 
@@ -39,9 +35,8 @@ class ProductsController < ApplicationController
     product.description = params[:new_description] || product.description
     product.save 
 
-    render json: {
-      Changed_to: product
-    }
+    render json: product.as_json(methods: [:readable_created_at, :readable_updated_at])
+    
   end 
 
   
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,26 @@
 class Product < ApplicationRecord
+
+  def readable_created_at
+    created_at.strftime("%A, %d %b %Y %l:%M %p") 
+  end
+  
+  def readable_updated_at
+    updated_at.strftime("%A, %d %b %Y %l:%M %p")
+  end 
+
+  def is_discounted? 
+    price < 10
+  end 
+
+  def tax 
+    price * 0.09
+  end
+  
+  def total 
+    price + tax
+  end 
+
 end
+
+
+
