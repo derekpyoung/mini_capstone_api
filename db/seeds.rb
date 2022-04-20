bc7bb7016700c4dbb08ab7a3890c2f8fb7d85df9
Derek Young <derekpyoung12@gmail.com>
2022-04-20 11:05:40 -0700

    change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 Product.create!([
-  {name: "Ninkasi", price: "8.0", img_url: "http://brewpublic.com/wp-content/uploads/2014/12/Ninkasi_Colorado.jpg", description: "great hoppy dark ipa"},
-  {name: "Guinness", price: "4.0", img_url: "http://barmagazine.co.uk/wp-content/uploads/2015/01/New-Guinness-Glass.jpg", description: "Belgian Strong Ale"},
-  {name: "Stella Artois", price: "4.0", img_url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.uneedabottle.ca%2Fwp-content%2Fuploads%2F2015%2F10%2FBottle_Stella_03.jpg8ebfa120-dcf8-46a8-aece-585f6791bb8bOriginal.jpg&f=1&nofb=1", description: "Belgian And French Ale"},
-  {name: "Lowenbrau", price: "8.0", img_url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Feluenheng.luenheng.com%2Fwp-content%2Fuploads%2F2021%2F02%2FLowenbrau.png&f=1&nofb=1", description: "Light Hybrid Beer"},
-  {name: "Blue Moon", price: "6.0", img_url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmir-s3-cdn-cf.behance.net%2Fproject_modules%2Fmax_1200%2F6fc1b143387853.57ed1c274aba9.jpg&f=1&nofb=1", description: "Stout"},
-  {name: "Tsingtao", price: "12.0", img_url: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.tsingtaobeer.co.za%2Fwp-content%2Fuploads%2F2017%2F10%2Fbottles.png&f=1&nofb=1", description: "Light Lager"},
-  {name: "Carlsberg", price: "12.0", img_url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fammangmart.com%2Fwp-content%2Fuploads%2F2020%2F05%2Fcarlsberg-650.jpg&f=1&nofb=1", description: "Strong Ale"},
-  {name: "Heineken", price: "11.0", img_url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.bandt.com.au%2Finformation%2Fuploads%2F2017%2F01%2FBottle_H3_2.jpg&f=1&nofb=1", description: "Belgian And French Ale"},
-  {name: "Paulaner", price: "5.0", img_url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.amstein.ch%2FHtdocs%2FImages%2FPictures%2F54787.jpg&f=1&nofb=1", description: "Stout"},
-  {name: "Rainier", price: "4.0", img_url: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwoodyboater.wpengine.netdna-cdn.com%2Fwp-content%2Fuploads%2F2015%2F05%2Frainierwallpaper.jpg&f=1&nofb=1", description: "nice cheap great lager"}
+  {name: "Ninkasi", price: "8.0", description: "great hoppy dark ipa"},
+  {name: "Guinness", price: "4.0", description: "Belgian Strong Ale"},
+  {name: "Stella Artois", price: "4.0", description: "Belgian And French Ale"},
+  {name: "Lowenbrau", price: "8.0", description: "Light Hybrid Beer"},
+  {name: "Blue Moon", price: "6.0", description: "Stout"},
+  {name: "Tsingtao", price: "12.0", description: "Light Lager"},
+  {name: "Carlsberg", price: "12.0", description: "Strong Ale"},
+  {name: "Heineken", price: "11.0", description: "Belgian And French Ale"},
+  {name: "Paulaner", price: "5.0",  description: "Stout"},
+  {name: "Rainier", price: "4.0", description: "nice cheap great lager"}
 ])
