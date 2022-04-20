95715b961f7b152e467519f9f414d078d514b7cf
Derek Young <derekpyoung12@gmail.com>
2022-04-20 11:08:44 -0700

    seeds with supplier

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 Product.create!([
-  {name: "Ninkasi", price: "8.0", description: "great hoppy dark ipa"},
-  {name: "Guinness", price: "4.0", description: "Belgian Strong Ale"},
-  {name: "Stella Artois", price: "4.0", description: "Belgian And French Ale"},
-  {name: "Lowenbrau", price: "8.0", description: "Light Hybrid Beer"},
-  {name: "Blue Moon", price: "6.0", description: "Stout"},
-  {name: "Tsingtao", price: "12.0", description: "Light Lager"},
-  {name: "Carlsberg", price: "12.0", description: "Strong Ale"},
-  {name: "Heineken", price: "11.0", description: "Belgian And French Ale"},
-  {name: "Paulaner", price: "5.0",  description: "Stout"},
-  {name: "Rainier", price: "4.0", description: "nice cheap great lager"}
+  {name: "Ninkasi", price: "8.0", description: "great hoppy dark ipa", supplier_id: 1},
+  {name: "Guinness", price: "4.0", description: "Belgian Strong Ale", supplier_id: 1},
+  {name: "Stella Artois", price: "4.0", description: "Belgian And French Ale", supplier_id: 1},
+  {name: "Lowenbrau", price: "8.0", description: "Light Hybrid Beer", supplier_id: 1},
+  {name: "Blue Moon", price: "6.0", description: "Stout", supplier_id: 1},
+  {name: "Tsingtao", price: "12.0", description: "Light Lager", supplier_id: 1},
+  {name: "Carlsberg", price: "12.0", description: "Strong Ale", supplier_id: 1},
+  {name: "Heineken", price: "11.0", description: "Belgian And French Ale", supplier_id: 1},
+  {name: "Paulaner", price: "5.0",  description: "Stout", supplier_id: 1},
+  {name: "Rainier", price: "4.0", description: "nice cheap great lager", supplier_id: 1}
 ])
