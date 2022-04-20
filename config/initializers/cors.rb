7a3556a10f4d4a9dc999b5149288cfedcab8e03a
Derek Young <derekpyoung12@gmail.com>
2022-04-20 11:35:56 -0700

    set up front and backend heroku netilify

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:8080", "mini-capstone.peterxjang.com"
+    origins "http://localhost:8080", "https://verdant-brioche-67a1b9.netlify.app"
 
     resource "*",
       headers: :any,
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,13 @@
+Product.all.destroy 
 Product.create!([
   {name: "Ninkasi", price: "8.0", description: "great hoppy dark ipa", supplier_id: 1},
   {name: "Guinness", price: "4.0", description: "Belgian Strong Ale", supplier_id: 1},
   {name: "Stella Artois", price: "4.0", description: "Belgian And French Ale", supplier_id: 1},
   {name: "Lowenbrau", price: "8.0", description: "Light Hybrid Beer", supplier_id: 1},
-  {name: "Blue Moon", price: "6.0", description: "Stout", supplier_id: 1},
+  {name: "Blue Moon", price: "6.0", description: "Stoasfgasfasdfut", supplier_id: 1},
   {name: "Tsingtao", price: "12.0", description: "Light Lager", supplier_id: 1},
   {name: "Carlsberg", price: "12.0", description: "Strong Ale", supplier_id: 1},
   {name: "Heineken", price: "11.0", description: "Belgian And French Ale", supplier_id: 1},
-  {name: "Paulaner", price: "5.0",  description: "Stout", supplier_id: 1},
+  {name: "Paulaner", price: "5.0",  description: "Stsdfgsdgout", supplier_id: 1},
   {name: "Rainier", price: "4.0", description: "nice cheap great lager", supplier_id: 1}
 ])
